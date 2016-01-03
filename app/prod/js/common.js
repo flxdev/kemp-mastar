34f6973b41d05dd38692a992eb4672a3efaabaa4
sweelemk <sweelemk@gmail.com>
2016-01-03 22:04:40 +0300

    template

--- app/prod/js/common.js
+++ app/prod/js/common.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
-	
+	//toggle goods
+	(function(){
+		$('.goods .btn').each(function(){
+			var this_ = $(this),
+				parent = this_.parents('.goods'),
+				slideBox = parent.find('.slide__box');
 
+			this_.on('click', function(){
+				if($(this).hasClass('active')){
+					return false;
+				} else if($(this).hasClass('btn__out')) {
+					$(this).addClass('active').siblings().removeClass('active');
+					slideBox.stop(true,true).slideUp(500);
+				} else {
+					$(this).addClass('active').siblings().removeClass('active');
+					slideBox.stop(true,true).slideDown(500);
+				}
+			});
+		});
+	})();
 
 });
