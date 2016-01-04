43d8baa120b50ebf3b1e3f3a047b94082562ea31
sweelemk <sweelemk@gmail.com>
2016-01-05 00:52:46 +0300

    news + search

--- app/dev/js/common.js
+++ app/dev/js/common.js
@@ -20,4 +20,96 @@ $(document).ready(function () {
 		});
 	})();
 
+	//tabs
+	(function(){
+		$('.tab__nav').each(function(){
+			var link = $(this).find('a'),
+				linkItem = $(this).find('li'),
+				index = link.data('href'),
+				parent = $(this).parents('.tab__container'),
+				tab_item = parent.find('.tab__item'),
+				link_cont = parent.find('.link__cont'),
+				slider = $('.sliders');
+
+			link.on('click', function(){
+				var this_ = $(this),
+					index = this_.data('href');
+
+				this_.parent().addClass('active').siblings().removeClass('active');
+				tab_item.fadeOut(0);				
+				if(parent.hasClass('news')){
+					parent.find("."+index).fadeIn(300).find('.sliders').slick('setPosition');
+				} else {
+					parent.find("."+index).fadeIn(300);
+				}
+				return false;
+			});
+			
+			linkItem.first().addClass('active');
+			parent.find("."+index).show();
+
+			link_cont.on('click', function(){
+				var this_ = $(this),
+					index = this_.data('href');
+
+				this_.parents('.tab__content').find('.' + index).fadeIn(300).siblings().fadeOut(0);
+				this_.parents('.tab__container').find('a[data-href=' + index + ']').parent().addClass('active').siblings().removeClass('active');
+			});
+		});
+	})();
+
+	//input autocomplete
+	(function(){
+		$('.tab__container').each(function(){
+			var item = $(this).find('.mark'),
+				value = item.text(),
+				parent = item.parents('.tab__item'),
+				input = parent.find('.input');
+
+			item.on('click', function(){
+				var value = $(this).text();
+
+				input.val(value);
+
+				return false;
+			});
+		});
+	})();
+
+	//slick init
+	(function(){
+		var slider = $('.sliders');
+
+		if(slider.length) {
+			slider.each(function(){
+				var slider = $(this);
+				var nameClass = slider.parents('.row').attr('class').split(' ')[0];
+
+				console.log(slider)
+
+				if($('.' + nameClass).hasClass('slider')) {
+					slider.slick({
+						arrows: false,
+						slidesToShow: 4,
+						slidesToScroll: 1,
+						speed: 500,
+						infinite: true
+					});
+				} else {
+					slider.slick();
+
+					$('.' + nameClass + ' .slide__prev').on('click', function(){
+						slider.slick('slickPrev');
+					});
+			
+					$('.' + nameClass + ' .slide__next').on('click', function(){
+						slider.slick('slickNext');
+					});
+				}				
+			});
+		}
+
+	})();
+
+
 });
