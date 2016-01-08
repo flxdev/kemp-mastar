a410f28817397cf40f6d2ce01f56f4c9a6e0d05b
sweelemk <sweelemk@gmail.com>
2016-01-08 17:29:20 +0300

    2-2catalog-razdel-tovar

--- app/dev/js/common.js
+++ app/dev/js/common.js
@@ -184,6 +184,7 @@ $(document).ready(function () {
 					val += 1;
 					input.val(val);
 				}
+				input.trigger('change');
 			});
 			minus.on('click', function(){
 				var val = +(input.val());
@@ -195,9 +196,35 @@ $(document).ready(function () {
 					input.val('1');
 					return false;
 				}
+				input.trigger('change');
 			});
 		});
 	})();
+
+	//spinner count
+	$('.js-price').each(function() {
+		$('.spinner__input').on('change', function() {
+			$(this).parents('.js-price').find('.result').text($(this).val()*$(this).parents('.js-price').find('.result').data('price'));
+			$('.js-price-text').map(function() {
+				$(this).text($(this).text().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+			})
+		});
+	});
+
+	//prices
+	function prices() {
+		$(window).ready(function() {
+			$('.js-price').each(function() {
+				var val = $('.spinner__input').val();
+				$(this).find('.result').text(val*$(this).find('.result').data('price'));
+			});
+
+			$('.js-price-text').map(function() {
+				$(this).text($(this).text().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+			});
+		});
+	};
+	prices();
 
 	//fixed tab tab
 	function fakePrice() {
@@ -223,6 +250,41 @@ $(document).ready(function () {
 			speed: 375,
 			moreLink: '<a href="#">Развернуть всё описание</a>',
 			lessLink: '<a href="#">Свернуть описание</a>'
+		});
+	})();
+
+	//product slider
+	(function(){
+		$('.slider-for').slick({
+			arrows: false,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			speed: 500,
+			infinite: true,
+			asNavFor: '.slider-nav',
+			fade: true
+		});
+		$('.slider-nav').slick({
+			arrows: false,
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			speed: 500,
+			infinite: true,
+			asNavFor: '.slider-for',
+			focusOnSelect: true
+		});
+
+	})();
+
+	//popup slider
+	(function(){
+		$('.popup__slider').fancybox({
+			padding: 0,
+			tpl: {
+				closeBtn : '<a class="fancybox-item fancybox-close" href="javascript:;"><svg viewBox="0 0 28.5 28.5" xmlns="http://www.w3.org/2000/svg"><path id="svg_1" d="m28.276947,2.974928l-2.830002,-2.83l-11.169999,11.17l-11.169998,-11.17l-2.83,2.83l11.169998,11.17l-11.169998,11.169998l2.83,2.830002l11.169998,-11.17l11.169999,11.17l2.830002,-2.830002l-11.170002,-11.169998l11.170002,-11.17z"/></svg></a>',
+				next: '<a title="Next" class="fancybox-nav fancybox-next" href="javascript:;"><span><svg viewBox="0 0 286 512" xmlns="http://www.w3.org/2000/svg"><path d="m16.600006,512c-4.099998,0 -8.099998,-1.600006 -11.099998,-4.799988c-5.700005,-6.100006 -5.400002,-15.700012 0.799995,-21.5l241.199997,-224.5l-241.5,-235.100012c-6,-5.9 -6.099998,-15.5 -0.300003,-21.5c5.900002,-6 15.5,-6.1 21.5,-0.3l252.800003,246.2c3,2.899994 4.700012,6.899994 4.600006,11.100006c-0.100006,4.199982 -1.800018,8.100006 -4.800018,10.899994l-252.899994,235.399994c-2.899994,2.800018 -6.599991,4.100006 -10.299988,4.100006z"/></svg></span></a>',
+				prev: '<a title="Previous" class="fancybox-nav fancybox-prev" href="javascript:;"><span><svg viewBox="0 0 286 512" xmlns="http://www.w3.org/2000/svg"><path d="m268.5,512c-3.700012,0 -7.399994,-1.299988 -10.299988,-4.100006l-253.000015,-235.399994c-3,-2.799988 -4.800003,-6.799988 -4.800003,-10.899994c-0.099998,-4.200012 1.600006,-8.200012 4.600006,-11.100006l252.899994,-246.2c6,-5.8 15.600006,-5.7 21.5,0.3c5.799988,6 5.700012,15.600001 -0.299988,21.5l-241.5,235.100012l241.199982,224.5c6.100006,5.700012 6.5,15.299988 0.800018,21.5c-3,3.199982 -7.100006,4.799988 -11.100006,4.799988z"/></svg></span></a>'
+			}
 		});
 	})();
 
